f9e5ff467743f6f83a51bbf1168aaf1b28a600db
ddavis-2015 <ddavis@bdti.com>
2021-02-18 07:09:52 -0800

    micro: port operator FLOOR_DIV kernel from lite with test
    Complete implementation of TFLM operator FLOOR_DIV and associated TFLM test code.
    
    This represents PR step 5 of the work to port operator FLOOR_DIV as tracked in Issue #45657

--- tensorflow/lite/micro/kernels/micro_ops.h
+++ tensorflow/lite/micro/kernels/micro_ops.h
@@ -60,6 +60,7 @@ TfLiteRegistration Register_COS();
 TfLiteRegistration Register_DEQUANTIZE();
 TfLiteRegistration Register_EQUAL();
 TfLiteRegistration Register_FLOOR();
+TfLiteRegistration Register_FLOOR_DIV();
 TfLiteRegistration Register_GREATER();
 TfLiteRegistration Register_GREATER_EQUAL();
 TfLiteRegistration Register_HARD_SWISH();
